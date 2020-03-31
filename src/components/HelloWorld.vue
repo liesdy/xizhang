34b870de3bdb6b381cdbd910089b726b5d71099e
洌水剑 <411476852@qq.com>
2020-03-31 15:40:58 +0800

    add scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="hello">
+    <div class="scss-test">
+      <div class="contain">
+        aaaaa
+      </div>
+    </div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -40,7 +45,12 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang='scss'>
+.scss-test {
+  .contain {
+    color: red;
+  }
+}
 h3 {
   margin: 40px 0 0;
 }
